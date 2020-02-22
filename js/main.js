31fbd398785d26e4a44b5b9b8ab43cf8e2d67ecc
Pavel Belkin <belkinpm@gmail.com>
2020-02-22 20:38:24 +0300

    оптимизирован код

--- js/main.js
+++ js/main.js
@@ -1,28 +1,16 @@
 /* создаем пустой массив для значений из полей ввода и присваиваем 
 переменной l количество таких полей */
-var myInputArr = [],
-    myInput = document.getElementsByClassName('my-input'),
+var myInput = document.querySelectorAll('.my-input'),
+    myWarn = document.querySelectorAll('.warning'),
     l = myInput.length,
-    myWarn = [],
     myButton = document.getElementById('btn'),
-    clearButton = document.getElementById('clrBtn'),
-    i;
-
-/* создаем функцию, которая наполняет значениями массивы */
-function saveInput() {
-    for (i = 0; i < l; i++) {
-        myInputArr[i] = +myInput[i].value;
-        myWarn[i] = document.getElementsByClassName('warning')[i];
-    }
-}
+    clearButton = document.getElementById('clrBtn');
 
 /* создаем функцию, которая проверяет число ли ввел пользователь и
 в зависимости от значения добавляет/удаляет значение класса */
 function checkInputType() {
-    saveInput();
-
-    for (i=0; i < l; i++) {
-        if (isNaN(myInputArr[i]) == true) {
+    for (var i=0; i < l; i++) {
+        if (isNaN(myInput[i].value) == true) {
             myWarn[i].classList.add("show");
             myInput[i].classList.add("red-border");
         } else {
@@ -32,34 +20,30 @@ function checkInputType() {
     }
 }
 
-function clearAll() {
-    saveInput();
+/* созадем функцию по проверке введеных значений в любом из полей ввода, 
+когда пользователь покидает это поле ввода */
+function checkAll() {
+    for (var i=0; i < l; i++) {
+        myInput[i].onblur = checkInputType;
+    }
+}
 
-    for (i=0; i < l; i++) {
+/* запускаем функцию по проверке введеных значений */
+checkAll(); 
+
+function clearAll() {
+    for (var i=0; i < l; i++) {
         myWarn[i].classList.remove("show");
         myInput[i].classList.remove("red-border");
     }
 }
 
-/* созадем функцию по проверке введеных значений в любом из полей ввода, 
-когда пользователь покидает это поле ввода */
-function checkAll() {
-    for (i=0; i < l; i++) {
-        myInput[i].onblur = checkInputType;
-    }
-}
-
-/* запускаем функцию по проверке введеных значений*/
-checkAll();
-
 /* создаем функцию по расчету и выведению бюджета*/
 function countBudget() {
-    i = 1;
     var myBudget = +myInput[0].value;
 
-    for (i=1; i<l; i++) {
-        myInputArr[i] = +myInput[i].value;
-        myBudget = myBudget - myInputArr[i]*2;
+    for (var i=1; i<l; i++) {
+        myBudget -= myInput[i].value*2;
     }
     if (isNaN(myBudget) == true) {
         alert('Please, check red fields and enter only numbers or leave a blank fields')
